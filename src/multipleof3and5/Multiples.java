f33638870942ebaf188b26aaed4c658c48eca6c0
aj <andrewjpack@gmail.com>
2019-02-08 14:43:57 +0000

    inital commit

---
+++ src/multipleof3and5/Multiples.java
@@ -0,0 +1,10 @@
+package multipleof3and5;
+
+public class Multiples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
